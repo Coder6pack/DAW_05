2a699a1dbd2e6f1fc642a233ccbbc360074bc41d
Kimngan136 <kimngandinhthi010@gmail.com>
2024-11-29 01:38:01 +0700

    định dạng tiền tệ

--- reacjs/src/components/product.js
+++ reacjs/src/components/product.js
@@ -5,6 +5,46 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import StarRating from './star_rating';
 function Product(props) {
+    const [selectedCapacity, setSelectedCapacity] = useState("");
+    const [selectedColor, setSelectedColor] = useState("");
+    const [currentPrice, setCurrentPrice] = useState("");
+    const [averageRating, setAverageRating] = useState(null);
+    const [availableColors, setAvailableColors] = useState([]);
+    const [currentDiscount, setCurrentDiscount] = useState("");
+    const [currentPercent, setCurrentPercent] = useState("");
+    const [selectedCapacityId, setSelectedCapacityId] = useState(null);
+
+    useEffect(() => {
+        if (!numeral.locales['vi-custom']) {
+            numeral.register('locale', 'vi-custom', {
+                delimiters: {
+                    thousands: '.',
+                    decimal: ',',
+                },
+                currency: {
+                    symbol: '',
+                },
+            });
+        }
+        numeral.locale('vi-custom');
+
+        if (props.member.product_detail && props.member.product_detail.length > 0) {
+            const firstDetail = props.member.product_detail[0];
+            setSelectedCapacity(firstDetail.capacity?.name || '');
+            setSelectedColor(firstDetail.color?.name || '');
+            setCurrentPrice(DoiThanhTien(firstDetail.price));
+            setSelectedCapacityId(firstDetail.capacity?.id);
+            const discountPrice = firstDetail.discount_detail.length > 0 ? DoiThanhTien(firstDetail.discount_detail[0].price) : '';
+            setCurrentDiscount(discountPrice);
+            const preCent = firstDetail.discount_detail.length > 0 ? firstDetail.discount_detail[0].percent : '';
+            setCurrentPercent(preCent);
+
+            updateAvailableColors(firstDetail.capacity?.name || '');
+            updateAverageRating(firstDetail.capacity?.id);
+        }
+    }, [props.member]);
+
+
    
     return (
         <>
