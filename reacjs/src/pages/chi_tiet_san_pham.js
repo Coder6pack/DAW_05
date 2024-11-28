1098293018aa9d5f039f126de889314839d73b7b
Kimngan136 <kimngandinhthi010@gmail.com>
2024-11-29 01:52:15 +0700

    file ctsp ở pages

---
+++ reacjs/src/pages/chi_tiet_san_pham.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import CTSanPham from "../components/chi_tiet_san_pham";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Loading from "../components/loading";
+
+function ChiTietSanPham() {
+   
+    return (
+        <>
+           
+        </>
+    );
+}
+
+export default ChiTietSanPham;
